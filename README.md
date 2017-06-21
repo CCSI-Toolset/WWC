40c7af9f9808b0323770ef51ae6707afe06453da
Val Hendrix <vchendrix@lbl.gov>
2017-06-21 15:17:05 -0700

    Initial import from 2016.02.00 release: svn r9987

---
+++ README.md
@@ -0,0 +1,37 @@
+# WWC
+**WWC: OpenFOAM Wetted Wall Simulation Package**
+A CFD model developed to simulate the hydrodynamics of falling film in wetted wall column, coupled with mass transfer and absorption of gas species.
+
+## Development Practices
+
+* Code development will be peformed in a forked copy of the repo. Commits will not be 
+  made directly to the ngt-archive repo. Developers will submit a pull 
+  request that is then merged by another team member, if another team member is available.
+* Each pull request should contain only related modifications to a feature or bug fix.  
+* Sensitive information (secret keys, usernames etc) and configuration data 
+  (e.g database host port) should not be checked in to the repo.
+* A practice of rebasing with the main repo should be used rather that merge commmits.
+
+## Getting Started
+
+TBD
+
+## Authors
+
+* Chao Wang
+* Zhijie (Jay) Xu
+
+See also the list of [contributors](https://github.com/CCSI-Toolset/WWC/contributors) who participated in this project.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, 
+see the [tags on this repository](https://github.com/WWC/tags). 
+
+## License
+
+See [LICENSE.md](LICENSE.md) file for details
+
+## Copyright Notice
+
+TBD
---
+++ WWC/interFoam.C
@@ -0,0 +1,149 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2013 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    interFoam
+
+Description
+    Solver for 2 incompressible, isothermal immiscible fluids using a VOF
+    (volume of fluid) phase-fraction based interface capturing approach.
+
+    The momentum and other fluid properties are of the "mixture" and a single
+    momentum equation is solved.
+
+    Turbulence modelling is generic, i.e. laminar, RAS or LES may be selected.
+
+    For a two-fluid approach see twoPhaseEulerFoam.
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+#include "MULES.H"
+#include "subCycle.H"
+#include "interfaceProperties.H"
+#include "twoPhaseMixture.H"
+#include "turbulenceModel.H"
+#include "pimpleControl.H"
+#include "fvIOoptionList.H"
+#include "Random.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+    #include "createTime.H"
+    #include "createMesh.H"
+
+    pimpleControl pimple(mesh);
+
+    #include "initContinuityErrs.H"
+    #include "createFields.H"
+    #include "readTimeControls.H"
+    #include "correctPhi.H"
+    #include "CourantNo.H"
+    #include "setInitialDeltaT.H"
+
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Random perturbation(12345);  
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.run())
+    {
+        #include "readTimeControls.H"
+        #include "CourantNo.H"
+        #include "alphaCourantNo.H"
+        #include "setDeltaT.H"
+
+        runTime++;
+
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        twoPhaseProperties.correct();
+
+        #include "alphaEqnSubCycle.H"
+        interface.correct();
+
+
+
+        // --- Pressure-velocity PIMPLE corrector loop
+        while (pimple.loop())
+        {
+            #include "UEqn.H"
+
+            // --- Pressure corrector loop
+            while (pimple.correct())
+            {
+                #include "pEqn.H"
+            }
+
+            if (pimple.turbCorr())
+            {
+                turbulence->correct();
+            }
+        }
+
+//	D1 = alpha1*D1L+(1-alpha1)*D1G;	
+	D1 = D1L*D1G/(D1G*alpha1+D1L*(1-alpha1));
+	D2 = alpha1*D2L;	
+	D3 = alpha1*D3L;	
+
+	Gama = D1*c_CO2*(1-Henry_const)/(alpha1+Henry_const*(1-alpha1));
+
+//	alphaFlux = mesh.magSf()*fvc::snGrad(alpha1);
+
+/* generating reaction source term fields */ 
+/*	const vectorField& centres(mesh.C()); 
+	
+	forAll(centres, celli) 
+	{ 
+	  
+	  dimensionedScalar r_source = RConst1*c1[celli]*c2[celli];
+//	  cout << fscale2.value() << endl << devX << endl <<  devY << endl << devZ << endl;
+
+	}
+*/
+
+	#include "c1Eqn.H"
+
+	#include "c2Eqn.H"
+
+	#include "c3Eqn.H"
+
+        runTime.write();
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
